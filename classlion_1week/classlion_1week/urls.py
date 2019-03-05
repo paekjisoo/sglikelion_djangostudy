a8af102af06323d5543c56343b45c147454229f9
paekjisoo <37473753+paekjisoo@users.noreply.github.com>
2019-03-06 04:50:54 +0900

    first commit

--- classlion_1week/classlion_1week/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-import myapp.views
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url('', myapp.views.home, name="home"),
-]
