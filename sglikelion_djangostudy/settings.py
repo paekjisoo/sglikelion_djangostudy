a28c326b6fdf7ae3721c556991bc72066884e9ce
paekjisoo <37473753+paekjisoo@users.noreply.github.com>
2019-03-06 10:03:39 +0900

    allowed hosts

--- sglikelion_djangostudy/settings.py
+++ sglikelion_djangostudy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1^s^b(39($am9@*u%k0ygq61%)ywmpx)ds7w*tie)e!%p_$x)m'
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
